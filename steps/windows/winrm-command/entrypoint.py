84bc30ad9a0ccbbf2d4741c76e85e7a6ea873c35
Itai Schwartz <itais@stackpulse.io>
2021-04-07 15:29:48 +0300

    add environment

--- steps/windows/winrm-command/entrypoint.py
+++ steps/windows/winrm-command/entrypoint.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
-s = winrm.Session('windows-host.example.com', auth=('john.smith', 'secret'))
-r = s.run_cmd('ipconfig', ['/all'])
+import env
+import shlex
+
+END_STRING = "<-- END -->"
+
+HOST = os.environ["HOST"]
+USERNAME = os.environ["USERNAME"]
+PASSWORD = os.environ["PASSOWRD"]
+COMMAND = os.environ["COMMAND"]
+
+commands = shlex.split(COMMAND)
+s = winrm.Session(HOST, auth=(USERNAME, PASSWORD))
+r = s.run_cmd(commands[0], commands[1:])
+
 print(r.status_code)
 print(r.std_out)
+print(r)
+output_dict = {"output": r.std_out, "status": r.status_code}
+print(f"{END_STRING}{json.dumps(output_dict)}")
