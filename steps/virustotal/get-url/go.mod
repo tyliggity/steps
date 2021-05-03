7e99bac944584ccb37d803ce5d5fa40558e604fb
Tyler Katz <tylerjkatz@gmail.com>
2021-05-03 16:23:24 -0400

    add virustotal get-url step

--- steps/virustotal/get-url/go.mod
+++ steps/virustotal/get-url/go.mod
@@ -2,6 +2,4 @@ module github.com/stackpulse/steps/steps/virustotal/get-url
 
 go 1.14
 
-require (
-	github.com/stackpulse/steps-sdk-go v0.0.0-20210314133745-61086c27983f
-)
+require github.com/stackpulse/steps-sdk-go v0.0.0-20210314133745-61086c27983f
--- steps/virustotal/get-url/main.go
+++ steps/virustotal/get-url/main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/stackpulse/steps-sdk-go/env"
 	"github.com/stackpulse/steps-sdk-go/step"
 )
 
 type VirustotalGetUrl struct {
-    VtApiKey string `env:"VT_API_KEY,required"`
-Url string `env:"URL,required"`
+	VtApiKey string `env:"VT_API_KEY,required"`
+	Url      string `env:"URL,required"`
 }
 
-type output struct {
-    Stats json `json:"stats"`
+type stepOutput struct {
+	Stats statsOutput `json:"stats"`
 	step.Outputs
+}
+
+type statsOutput struct {
+	Harmless   int `json:"harmless"`
+	Malicious  int `json:"malicious"`
+	Suspicious int `json:"suspicious"`
+	Timeout    int `json:"timeout"`
+	Undetected int `json:"undetected"`
+}
+
+type apiResponse struct {
+	Data struct {
+		Attributes struct {
+			LastAnalysisStats statsOutput `json:"last_analysis_stats"`
+			AsOwner           string      `json:"as_owner"`
+			Country           string      `json:"country"`
+			Reputation        int         `json:"reputation"`
+		} `json:"attributes"`
+	} `json:"data"`
 }
 
 func (s *VirustotalGetUrl) Init() error {
@@ -25,9 +49,52 @@ func (s *VirustotalGetUrl) Init() error {
 }
 
 func (s *VirustotalGetUrl) Run() (exitCode int, output []byte, err error) {
-    // Replace This Comment with Step Logic
+	//prepare request
+	reqURL := fmt.Sprintf("https://www.virustotal.com/api/v3/domains/%s", s.Url)
+	stepReq, err := http.NewRequest(http.MethodGet, reqURL, nil)
+	if err != nil {
+		return step.ExitCodeFailure, nil, err
+	}
+	stepReq.Header.Set("x-apikey", s.VtApiKey)
 
-	return step.ExitCodeOK, []byte{}, nil
+	//send request
+	res, err := http.DefaultClient.Do(stepReq)
+	if err != nil || res.StatusCode != http.StatusOK {
+		return step.ExitCodeFailure, nil, err
+	}
+
+	//parse response
+	defer res.Body.Close()
+	rawRes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return step.ExitCodeFailure, nil, err
+	}
+
+	var resp apiResponse
+	err = json.Unmarshal(rawRes, &resp)
+	if err != nil {
+		return step.ExitCodeFailure, nil, err
+	}
+
+	//unmarshal full response
+	var fullResp map[string]interface{}
+	err = json.Unmarshal(rawRes, &fullResp)
+	if err != nil {
+		return step.ExitCodeFailure, nil, err
+	}
+
+	//prepare output
+	out := stepOutput{
+		Stats:   resp.Data.Attributes.LastAnalysisStats,
+		Outputs: step.Outputs{Object: fullResp},
+	}
+	outputBytes, err := json.Marshal(out)
+	if err != nil {
+		return step.ExitCodeFailure, nil, err
+	}
+
+	//success
+	return step.ExitCodeOK, outputBytes, nil
 }
 
 func main() {
